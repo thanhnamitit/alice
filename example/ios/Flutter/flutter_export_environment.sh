f72905fc2bcb1d714b68b215ebd5413d94bfabe6
Nam <v.namdt2@vinid.net>
2020-11-25 14:40:41 +0700

    hi

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/namdt2/flutter/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/namdt2/Documents/flutter/merchant-app-onepos/alice/example"
+export "FLUTTER_APPLICATION_PATH=/Users/namdt2/Documents/flutter/merchant-app-platform-module/alice/example"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
